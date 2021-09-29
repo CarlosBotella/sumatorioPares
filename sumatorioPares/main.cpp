ac83db769473d1b63072fa1b12b8e01607efd007
cbotella <carlosb.n@icloud.com>
2021-09-29 12:36:52 +0200

    final sumatorioPares

--- sumatorioPares/main.cpp
+++ sumatorioPares/main.cpp
@@ -6,9 +6,24 @@
 //
 
 #include <iostream>
+using namespace std; // no hace falta poner "std::" delante de "cout" o "cin"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int sumatorioPares(){
+    int n;
+    int total = 0;
+    cout << "Elige un número: ";
+    cin >> n;
+    for (int i = 0; i <= n; i++) {
+        if (i%2) { // %2 comprueba si hay remainders
+            // cout << "i: " << i << "\n";
+            total = total + i + 1;
+            // cout << total << "\n";
+        }
+    }
+    cout << "resultado: " << total << "\n";
+    return total;
+}
+
+int main() {
+    sumatorioPares();
 }
